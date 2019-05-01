ad79e7e7304a04ea097994d5c0919a820ba87a51
wiswedel <wiswedel@users.noreply.github.com>
2019-05-01 13:51:23 +0200

    rewrite mouseKeys.sh (nested if out, case in)
    complete rewrite to replace nested if statements with case

--- mouseKeys.sh
+++ mouseKeys.sh
@@ -3,70 +3,111 @@
 currentWindow=$(xdotool getactivewindow getwindowname)
 inputKey=$1
 
-if [[ $inputKey == "mouseWheelPress" ]];
-then
 
-  if [[ $currentWindow =~ "Firefox" ]];
-   then
-      xte 'key F5' # reload page
+case "$inputKey" in
 
-   fi
+    mouseWheelPress)
+    
+        case "$currentWindow" in
+        
+            *"Firefox"*)
+                
+                xte 'key F5' # reload page
+            ;;
+        
+        esac    
+    
+    ;; # end of mouseWheelPress
 
-
-elif [[ $inputKey == "mouseWheelRight" ]];
-then
-
-   if [[ $currentWindow =~ "Firefox" ]];
-   then
-      xte 'keydown Control_L' 'key Tab' 'keyup Control_L' # next tab
-
-   elif [[ $currentWindow =~ "KMail" ]];
-   then
-      xte 'key Right' # next message
-
-   fi
-
-elif [[ $inputKey == "mouseWheelLeft" ]];
-then
-
-   if [[ $currentWindow =~ "Firefox" ]];
-   then
-      xte 'keydown Control_L' 'keydown Shift_L' 'key Tab' 'keyup Shift_L' 'keyup Control_L' # previous tab
-
-   elif [[ $currentWindow =~ "KMail" ]];
-   then
-      xte 'key Left' # previous message
-
-   fi
-
-elif [[ $inputKey == "thumbLower" ]];
-then
-
-   if [[ $currentWindow =~ "Firefox" ]];
-   then
-      xte 'keydown Alt_L' 'key Left' 'keyup Alt_L' # back
-
-   elif [[ $currentWindow =~ "KMail" ]];
-   then
-      xte 'key a' # reply to all
-
-   fi
-
-elif [[ $inputKey == "thumbUpper" ]];
-then
-
-   if [[ $currentWindow =~ "Firefox" ]];
-   then
-      xte 'keydown Control_L' 'key w' 'keyup Control_L' # close current window
-
-   elif [[ $currentWindow =~ "KMail" ]];
-   then
-      xte 'key Delete' # delete message
-
-   fi
-
-
-
-fi
-
-
+#################################################################################################################
+    
+    mouseWheelRight)
+    
+        case "$currentWindow" in
+        
+            *"Kate"*)
+                
+                xte 'keydown Alt_L' 'key Right' 'keyup Alt_L' # next tab
+            ;;
+            
+            *"Firefox"*)
+            
+                xte 'keydown Control_L' 'key Tab' 'keyup Control_L' # next tab
+            ;;
+            
+            *"KMail"*)
+                
+                xte 'key Right' # next message
+            ;;            
+        
+        esac
+        
+    ;; # end of mouseWheelRight
+    
+#################################################################################################################
+    
+    mouseWheelLeft)
+    
+        case "$currentWindow" in
+        
+            *"Kate"*)
+                
+                xte 'keydown Alt_L' 'key Left' 'keyup Alt_L' # previous tab
+            ;;
+            
+            *"Firefox"*)
+            
+                xte 'keydown Control_L' 'keydown Shift_L' 'key Tab' 'keyup Shift_L' 'keyup Control_L' # previous tab
+            ;;
+            
+            *"KMail"*)
+                
+                xte 'key Left' # previous message
+            ;;
+        
+        esac
+        
+    ;; # end of mouseWheelLeft
+    
+#################################################################################################################
+    
+    thumbLower)
+    
+        case "$currentWindow" in
+            
+            *"Firefox"*)
+            
+                xte 'keydown Alt_L' 'key Left' 'keyup Alt_L' # back
+            ;;
+            
+            *"KMail"*)
+                
+                 xte 'key a' # reply to all
+            ;;
+            
+        esac
+    
+    ;; # end of thumbLower
+    
+#################################################################################################################
+    
+    thumbUpper)
+    
+        case "$currentWindow" in
+            
+            *"KMail"*)
+                
+                 xte 'key Delete' # delete message
+            ;;
+            
+            
+            *)
+            
+                xte 'keydown Control_L' 'key w' 'keyup Control_L' # close current window
+            ;;
+        
+        esac
+    
+    ;; # end of thumbUpper
+    
+esac
